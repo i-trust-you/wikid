ba5cd53b980e3a133fb56f815ec38a26c6103ab5
YeongTaek <yeongtaek2109@gmail.com>
2024-06-26 00:01:26 +0900

    feat : button Index 및 실행

---
+++ app/_components/common/button/index.tsx
@@ -0,0 +1,9 @@
+import ButtonClick from "./ButtonClick";
+import Root from "./Root";
+
+const Button = {
+	Root,
+	ButtonClick,
+};
+
+export default Button;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Button from "./_components/common/button";
+
 export default function Page() {
-	return <></>;
+	return (
+		<>
+			<Button.Root>
+				<Button.ButtonClick>불에 탄 버터 버튼</Button.ButtonClick>
+			</Button.Root>
+		</>
+	);
 }
