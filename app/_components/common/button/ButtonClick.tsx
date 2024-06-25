f84706afa5b2994f1fb6d3af1c29eb16f0af04aa
YeongTaek <yeongtaek2109@gmail.com>
2024-06-26 00:15:58 +0900

    style : 속성에 따른 스타일링

--- app/_components/common/button/ButtonClick.tsx
+++ app/_components/common/button/ButtonClick.tsx
@@ -9,6 +9,25 @@ interface ButtonProps extends PropsWithChildren {}
 
 const ButtonClick: React.FC<ButtonProps> = ({ children }) => {
 	const { href, onClick, type, disabled } = useButtonContext();
+
+	let buttonClasses = "relative h-full w-full gap-[10px] rounded-[10px] text-md font-semibold border";
+
+	if (disabled) {
+		buttonClasses += " opacity-50 cursor-not-allowed";
+	} else {
+		buttonClasses += " cursor-pointer";
+	}
+
+	switch (type) {
+		case "button":
+		case "submit":
+			buttonClasses += " bg-primary-200 text-white";
+			break;
+		case "reset":
+			buttonClasses += " bg-white border-primary-200 text-primary-200";
+			break;
+		default:
+			buttonClasses += " bg-red-200 text-white";
 	}
 
 	return href ? (
