feb8d1a1cbd98b3cae76469f65589b754a8d0a8b
User850413 <csyou1228@gmail.com>
2024-06-26 16:17:28 +0900

    feat : BackLink 스타일링

--- app/_components/common/BackLink/index.tsx
+++ app/_components/common/BackLink/index.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
+
+import LinkIcon from "../../../../public/icons/link.svg";
 
 interface Props {
 	children: string;
@@ -9,7 +12,10 @@ interface Props {
 export default function BackLink({ children }: Props) {
 	const copyTextToClipboard = async (text: string) => {
 		try {
-			await navigator.clipboard.writeText(text);
+			await navigator.clipboard.writeText(text); // 클립보드에 복사
+
+			// 상태 표시 코드
+			//
 		} catch (error) {
 			console.error("클립보드 복사 실패");
 		}
@@ -19,5 +25,10 @@ export default function BackLink({ children }: Props) {
 		copyTextToClipboard(children);
 	}
 
-	return <button onClick={onClickCopy}>{children}</button>;
+	return (
+		<button className="flex items-center gap-[5px] rounded-[10px] bg-[#EEF9F6] p-[5px_10px] text-md text-[#4CBFA4]" onClick={onClickCopy}>
+			<Image src={LinkIcon} alt="링크 아이콘" />
+			<span>{children}</span>
+		</button>
+	);
 }
