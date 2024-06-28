5e5232a54d1e035ad0a133b93f83f1304b8f934c
heony704 <heony704@gmail.com>
2024-06-29 08:09:12 +0900

    feat: API 연결

--- app/wikilist/_components/ProfileCard.tsx
+++ app/wikilist/_components/ProfileCard.tsx
@@ -2,28 +2,41 @@ import Image from "next/image";
 
 type ProfileCardProps = {
 	profile: {
-		image: string;
-		name: string;
-		area: string;
+		updatedAt: string;
 		job: string;
-		url: string;
+		nationality: string;
+		city: string;
+		image: string | null;
+		code: string;
+		name: string;
+		id: number;
 	};
 };
 
 export default function ProfileCard({ profile }: ProfileCardProps) {
-	const { image, name, area, job, url } = profile;
+	const { image, name, city, nationality, job, code } = profile;
+	const defaultProfileImageUrl = "/icons/profile.svg";
 
 	return (
 		<div className="flex items-start rounded-[10px] px-[25px] py-[21px] shadow-[0_4px_20px_0_rgba(0,0,0,0.08)] tablet:items-center tablet:px-9 tablet:py-6">
-			<Image src={image} width={60} height={60} alt="프로필 사진" className="h-[60px] rounded-full object-cover tablet:h-[85px] tablet:w-[85px]" />
+			<Image
+				src={image || defaultProfileImageUrl}
+				width={60}
+				height={60}
+				alt="프로필 사진"
+				className="h-[60px] rounded-full object-cover tablet:h-[85px] tablet:w-[85px]"
+			/>
 			<div className="ml-5 flex grow flex-col gap-[14px] tablet:flex-row tablet:items-end">
-				<div className="">
+				<div>
 					<h3 className="mb-[10px] text-2xl font-semibold text-gray-500 tablet:mb-[14px]">{name}</h3>
-					<p className="text-md font-normal text-gray-400">{area}</p>
+					<p className="text-md font-normal text-gray-400">
+						{city}
+						{nationality && `, ${nationality}`}
+					</p>
 					<p className="text-md font-normal text-gray-400">{job}</p>
 				</div>
 				{/* TODO: url을 Backlink 컴포넌트로 변경 */}
-				<div className="grow text-right">{url}</div>
+				<div className="grow text-right">{code}</div>
 			</div>
 		</div>
 	);
--- app/wikilist/page.tsx
+++ app/wikilist/page.tsx
@@ -1,47 +1,49 @@
 "use client";
 
+import API from "@/_api";
 import ProfileCard from "@/wikilist/_components/ProfileCard";
 import SearchBar from "@/wikilist/_components/SearchBar";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+type Profile = {
+	totalCount: number;
+	list: {
+		updatedAt: string;
+		job: string;
+		nationality: string;
+		city: string;
+		image: string | null;
+		code: string;
+		name: string;
+		id: number;
+	}[];
+};
 
 export default function Page() {
 	const [name, setName] = useState<string>("");
+	const [profiles, setProfiles] = useState<Profile["list"] | undefined>();
+	const [totalProfiles, setTotalProfiles] = useState<number>(0);
 
 	const handleSearchBar = (value: string) => {
-		console.log(value);
 		setName(value);
 	};
 
-	const profiles = [
-		{
-			image: "/images/not_found.png",
-			name: "김동욱",
-			area: "서울, 대한민국",
-			job: "대학생",
-			url: "https://www.wikied.kr/kimdong",
-		},
-		{
-			image: "/images/not_found.png",
-			name: "김동욱",
-			area: "서울, 대한민국",
-			job: "대학생",
-			url: "https://www.wikied.kr/kimdong",
-		},
-		{
-			image: "/images/not_found.png",
-			name: "김동욱",
-			area: "서울, 대한민국",
-			job: "대학생",
-			url: "https://www.wikied.kr/kimdong",
-		},
-	];
+	useEffect(() => {
+		const getProfiles = async (keyword: string) => {
+			const response = await API["{teamId}/profiles"].GET({ teamId: "6-11", page: 1, pageSize: 3, name: keyword });
+			setTotalProfiles(response.totalCount);
+			setProfiles(response.list);
+		};
+
+		getProfiles(name);
+	}, [name]);
 
 	return (
 		<main className="flex h-full min-h-screen justify-center px-[21px] py-[80px] tablet:px-6">
 			<div className="flex w-full max-w-[860px] flex-col">
 				<SearchBar onChange={handleSearchBar} />
-				{profiles.length < 1 ? (
+				{profiles === undefined || profiles.length < 1 ? (
 					<div className="flex grow flex-col items-center justify-center">
 						<p className="text-2lg font-medium text-gray-400">"{name}"과 일치하는 검색 결과가 없어요.</p>
 						<Image src="/images/search.png" width={108} height={108} alt="검색 사진" className="mt-[35px] tablet:h-[144px] tablet:w-[144px]" />
@@ -49,12 +51,10 @@ export default function Page() {
 				) : (
 					<>
 						<p className="mt-4 text-md font-normal text-gray-400">
-							"{name}"님을 총 <span className="text-primary-300">{profiles.length}</span>명 찾았습니다.
+							"{name}"님을 총 <span className="text-primary-300">{totalProfiles}</span>명 찾았습니다.
 						</p>
 						<div className="mt-10 flex flex-col gap-6 tablet:mt-[57px]">
-							{profiles.map((profile) => (
-								<ProfileCard profile={profile} />
-							))}
+							{profiles?.map((profile) => <ProfileCard key={profile.id} profile={profile} />)}
 							{/* TODO: pagination 컴포넌트로 변경 */}
 							<div className="mt-[54px] flex justify-center tablet:mt-20 desktop:mt-[120px]">pagination</div>
 						</div>
