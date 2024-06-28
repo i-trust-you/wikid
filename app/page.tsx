38e9f009c6ca0121f92292242a4fbefb681724b1
YeongTaek <yeongtaek2109@gmail.com>
2024-06-28 14:45:44 +0900

    refector : Button 스타일 속성 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,39 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
+import Button from "./_components/common/Button";
+
 export default function Page() {
-	return <></>;
+	const router = useRouter();
+	const handleClick = () => {
+		alert("회원가입됐습니다");
+		router.push("/login");
+	};
+
+	const isSubmitted = "" || "" || !"";
+
+	return (
+		<div className="flex flex-col gap-[10px]">
+			<div className="w-50 relative h-10">
+				<Button href="/board" buttonStyle="outline">
+					목록으로
+				</Button>
+			</div>
+			<div className="w-50 relative h-10">
+				<Button onClick={handleClick}>회원가입</Button>
+			</div>
+			<div className="w-50 relative h-10">
+				<Button disabled={isSubmitted} buttonStyle="basic">
+					로그인
+				</Button>
+			</div>
+			<div className="w-50 relative h-10">
+				<Button href="/board" buttonStyle="cancel">
+					취소
+				</Button>
+			</div>
+		</div>
+	);
 }
