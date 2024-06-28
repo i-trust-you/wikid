24a39b04b74151f2b578affe0f18561ccbc892ff
dotw0xff <dotw0xff@gmail.com>
2024-06-28 13:10:39 +0900

    (임시커밋)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import Form from "@/_components/common/Form/Form";
+import Input from "@/_components/common/Form/Input";
+
 export default function Page() {
-	return <></>;
+	return (
+		<div>
+			<Form
+				onSubmit={(data) => console.log(data)}
+				validationFunction={function (formData: Record<string, string>): Record<string, string> {
+					let errorRecord: Record<string, string> = { email: "wrong Email" } as Record<string, string>;
+					return errorRecord;
+				}}
+			>
+				<Input name="name" type="text" />
+				<Input name="email" label="Email" type="email" />
+				<button type="submit">Submit</button>
+			</Form>
+		</div>
+	);
 }
