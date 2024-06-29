1b896ab28553b10094615c2c16d160e30087d011
heony704 <heony704@gmail.com>
2024-06-29 16:27:04 +0900

    refactor: props 수정

--- public/icons/AlignCenterIcon.tsx
+++ public/icons/AlignCenterIcon.tsx
@@ -1,9 +1,15 @@
-export default function AlignCenterIcon({ fill, ...props }: React.SVGProps<SVGSVGElement>) {
+interface IconProps {
+	width: string;
+	height: string;
+	color?: string;
+}
+
+export default function AlignCenterIcon({ width, height, color = "#8F95B2" }: IconProps) {
 	return (
-		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+		<svg width={width} height={height} viewBox={[0, 0, width, height].join("\u0020")} fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
 				d="M5 18V16.8333H19V18H5ZM8.18889 14.7917V13.625H15.8306V14.7917H8.18889ZM5 11.5833V10.4167H19V11.5833H5ZM8.18889 8.375V7.20833H15.8306V8.375H8.18889ZM5 5.16667V4H19V5.16667H5Z"
-				fill={fill || "#8F95B2"}
+				fill={color}
 			/>
 		</svg>
 	);
--- public/icons/AlignRightIcon.tsx
+++ public/icons/AlignRightIcon.tsx
@@ -1,9 +1,15 @@
-export default function AlignRightIcon({ fill, ...props }: React.SVGProps<SVGSVGElement>) {
+interface IconProps {
+	width: string;
+	height: string;
+	color?: string;
+}
+
+export default function AlignRightIcon({ width, height, color = "#8F95B2" }: IconProps) {
 	return (
-		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+		<svg width={width} height={height} viewBox={[0, 0, width, height].join("\u0020")} fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
 				d="M5 18V16.8333H19V18H5ZM9.84167 14.7917V13.625H19V14.7917H9.84167ZM5 11.5833V10.4167H19V11.5833H5ZM9.84167 8.375V7.20833H19V8.375H9.84167ZM5 5.16667V4H19V5.16667H5Z"
-				fill={fill || "#8F95B2"}
+				fill={color}
 			/>
 		</svg>
 	);
