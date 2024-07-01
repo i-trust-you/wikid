f629cc7d9c7b3b3b30f2d26798c150e3e34950a1
Sombian <sombian.software@gmail.com>
2024-07-01 19:12:40 +0900

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,11 @@ import "./globals.css";
 export default function Layout({ children }: Readonly<React.PropsWithChildren>) {
 	return (
 		<html lang="ko">
-			<body>
-				{children}
-				<div id="toast" />
-				<div id="alert" />
-				<div id="modal" />
-			</body>
+			<Header />
+			<body>{children}</body>
+			<div id="toast" />
+			<div id="alert" />
+			<div id="modal" />
 		</html>
 	);
 }
