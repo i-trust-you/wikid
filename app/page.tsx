2495a4136b5de15c4a7a5d198902b1765f85a0cb
Sombian <sombian.software@gmail.com>
2024-07-01 14:15:05 +0900

    fix: threshold & bottleneck

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Page() {
 	return (
 		<main className="flex h-screen w-screen items-center justify-center">
 			<div className="h-[250px] w-[500px]">
-				<Swiper gap={10} columns={4} sensitivity={0.45}>
+				<Swiper gap={10} columns={4} threshold={0.45}>
 					{new Array(7).fill(null).map((_, index) => (
-						<div key={index} className="flex items-center justify-center rounded-[10px] bg-gray-300 select-none">
+						<div key={index} className="flex select-none items-center justify-center rounded-[10px] bg-gray-300">
 							{index}
 						</div>
 					))}
