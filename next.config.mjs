3de0f4d8052182bdb7a980c5b26855737f2ea623
heony704 <heony704@gmail.com>
2024-07-02 14:58:38 +0900

    feat: config에 이미지 주소 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export /
 /** @type {import("next").NextConfig} */
-export default {};
+export default {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
+};
