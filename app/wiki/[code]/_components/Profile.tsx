0f79960f5d78ad1e5269aef61a51cffbf3b32922
heony704 <heony704@gmail.com>
2024-07-04 04:16:50 +0900

    feat: Profile 추가

---
+++ app/wiki/[code]/_components/Profile.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import useMediaQuery from "@/_hooks/useMediaQuery";
+
+import ArrowDownIcon from "../../../../public/icons/ArrowDownIcon";
+
+type ProfileData = {
+	image: string | null;
+	city: string;
+	mbti: string;
+	job: string;
+	sns: string;
+	birthday: string;
+	nickname: string;
+	bloodType: string;
+	nationality: string;
+};
+
+type Props = {
+	profile: ProfileData;
+};
+
+export default function Profile({ profile }: Props) {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const isLargeScreen = useMediaQuery("(min-width: 1200px)");
+
+	return (
+		<div className="flex w-full flex-col rounded-[10px] bg-white px-5 py-[15px] shadow-basic tablet:px-[30px] tablet:py-5 desktop:w-[320px] desktop:py-[60px]">
+			<div className="flex desktop:flex-col">
+				<Image
+					src={profile?.image ?? "/images/not_found.png"}
+					width={200}
+					height={200}
+					alt="프로필 이미지"
+					className="h-full max-h-[62px] w-full max-w-[62px] rounded-full object-cover tablet:max-h-[71px] tablet:max-w-[71px] desktop:m-auto desktop:max-h-[200px] desktop:max-w-[200px]"
+				/>
+				<table className="ml-5 grow desktop:mt-[60px] desktop:grow-0">
+					<tbody className="flex flex-col gap-2 text-left text-xs font-normal tablet:text-md desktop:gap-4">
+						<tr>
+							<th className="w-[60px] font-normal text-gray-400">거주 도시</th>
+							<td className="pl-[10px] text-gray-500">{profile.city}</td>
+						</tr>
+						<tr>
+							<th className="w-[60px] font-normal text-gray-400">MBTI</th>
+							<td className="pl-[10px] text-gray-500">{profile.mbti}</td>
+						</tr>
+						<tr>
+							<th className="w-[60px] font-normal text-gray-400">직업</th>
+							<td className="pl-[10px] text-gray-500">{profile.job}</td>
+						</tr>
+						{(isLargeScreen || isOpen) && (
+							<>
+								<tr>
+									<th className="w-[60px] font-normal text-gray-400">SNS 계정</th>
+									<td className="pl-[10px] text-gray-500">{profile.sns}</td>
+								</tr>
+								<tr>
+									<th className="w-[60px] font-normal text-gray-400">생일</th>
+									<td className="pl-[10px] text-gray-500">{profile.birthday}</td>
+								</tr>
+								<tr>
+									<th className="w-[60px] font-normal text-gray-400">별명</th>
+									<td className="pl-[10px] text-gray-500">{profile.nickname}</td>
+								</tr>
+								<tr>
+									<th className="w-[60px] font-normal text-gray-400">혈액형</th>
+									<td className="pl-[10px] text-gray-500">{profile.bloodType}</td>
+								</tr>
+								<tr>
+									<th className="w-[60px] font-normal text-gray-400">국적</th>
+									<td className="pl-[10px] text-gray-500">{profile.nationality}</td>
+								</tr>
+							</>
+						)}
+					</tbody>
+				</table>
+			</div>
+			{!isLargeScreen && (
+				<div
+					className={`${isOpen && "rotate-180"} m-auto cursor-pointer rounded-[5px] transition hover:bg-gray-100 active:bg-gray-200`}
+					onClick={() => setIsOpen((prev) => !prev)}
+				>
+					<ArrowDownIcon width="24" height="24" />
+				</div>
+			)}
+		</div>
+	);
+}
---
+++ app/wiki/[code]/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Profile from "@/wiki/[code]/_components/Profile";
+
+import BackLink from "@/_components/common/BackLink";
+import Button from "@/_components/common/Button";
+
+export default function Page() {
+	const test = {
+		image: null,
+		city: "전주",
+		mbti: "ESFJ",
+		job: "home protector",
+		sns: "heony704",
+		birthday: "1997-07-04",
+		nickname: "허니",
+		bloodType: "A",
+		nationality: "한국",
+	};
+
+	return (
+		<main className="desktop:pr-[400px]">
+			<div className="relative m-auto max-w-[860px] px-5 py-10 tablet:px-[60px] tablet:py-[60px]">
+				<div className="flex justify-between">
+					<h1 className="text-3xl font-semibold text-gray-500 tablet:text-5xl">이승헌</h1>
+					<div className="w-[120px] tablet:w-[160px]">
+						<Button> 위키 참여하기</Button>
+					</div>
+				</div>
+				<div className="mt-6 tablet:mt-8">
+					<BackLink>https://dkjfaklfd</BackLink>
+				</div>
+				<div className="mt-3 flex tablet:mt-[15px] desktop:absolute desktop:-right-[320px] desktop:top-0 desktop:mt-10">
+					<Profile profile={test} />
+				</div>
+				<div className="mt-10 tablet:mt-[60px]">개요</div>
+			</div>
+		</main>
+	);
+}
