22c5d4ad4abe553a87bbd45f3597cd4cf53c4b7f
오영택 <87858689+Programmeryeongtaek@users.noreply.github.com>
2024-07-04 11:20:30 +0900

    feat : View post page (#24)
    * feat : boxShdaow 스타일 추가
    
    * feat : 게시글 상세 페이지 UI 구성 세팅
    
    * Merge branch 'main' of https://github.com/i-trust-you/wikid into view-post-page
    
    * style : boxShadow 값 수정
    
    * Merge branch 'main' of https://github.com/i-trust-you/wikid into view-post-page
    
    * feat : Comment 컴포넌트
    
    * feat : borard/{boardId} Page
    
    * remove : 함수 위치 변경
    
    * feat : Aricle 컴포넌트
    
    * docs : 이미지 받아오는 경로 설정
    
    * Merge - form, dropdown
    
    * decs : 폴더 경로 변경
    
    * feat: 여백 다시 복귀
    
    * feat: 게시글 상세 페이지의 데이터 페치/렌더링 로직 구현
    
    * chore: 게시물 상세 페이지와 연관된 타입 문제 교정
    
    * feat: 폴더 이름 변경
    
    * fix: module not found
    
    ---------
    
    Co-authored-by: heony704 <heony704@gmail.com>
    Co-authored-by: dotw0xff <dotw0xff@gmail.com>
    Co-authored-by: dotw0xff <154291841+dotw0xff@users.noreply.github.com>

---
+++ app/boards/[id]/page.tsx
@@ -0,0 +1,19 @@
+import Article from "@/boards/[id]/_components/Article";
+import CommentList from "@/boards/[id]/_components/CommentList";
+
+import Button from "@/_components/common/Button";
+
+export default function Page(props: { params: { slug: number } }) {
+	const articleId = props.params.slug;
+	return (
+		<div className="m-auto flex w-[335px] flex-col items-center gap-10 tablet:w-[624px] desktop:w-[1060px]">
+			<Article articleId={articleId} />
+			<div className="h-[45px] w-[140px]">
+				<Button href="/boards" style="outline">
+					목록으로
+				</Button>
+			</div>
+			<CommentList articleId={articleId} />
+		</div>
+	);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -42,6 +42,9 @@ export default {
 			yellow: "#FDD181",
 			transparent: "transparent",
 		},
+		boxShadow: {
+			basic: '0px 4px 20px 0px rgba(0, 0, 0, 0.08)',
+		}
 	},
 	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
 };
