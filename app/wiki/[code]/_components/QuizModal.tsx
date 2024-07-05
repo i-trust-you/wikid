0e7c3390c16895d5fdccc8b6975f614c52b78ff3
heony704 <heony704@gmail.com>
2024-07-05 09:10:02 +0900

    feat: 퀴즈모달 401 에러 핸들링

--- app/wiki/[code]/_components/QuizModal.tsx
+++ app/wiki/[code]/_components/QuizModal.tsx
@@ -17,7 +17,7 @@ type Props = {
 };
 
 export default function QuizModal({ code, question, onSuccess }: Props) {
-	const [isError, setIsError] = useState<Boolean>(false);
+	const [errorMessage, setErrorMessage] = useState<string>("");
 	const [value, setValue] = useState<string>("");
 
 	const [accessToken] = useLocalStorage<string | null>("accessToken", null);
@@ -34,12 +34,15 @@ export default function QuizModal({ code, question, onSuccess }: Props) {
 		await API["{teamId}/profiles/{code}/ping"]
 			.POST({ teamId: "6-11", code }, { securityAnswer: value })
 			.then((response) => {
-				setIsError(false);
+				setErrorMessage("");
 				onSuccess();
 				Modal.close();
 			})
-			.catch(() => {
-				setIsError(true);
+			.catch((error) => {
+				if (error.response && error.response.status === 401) {
+					setErrorMessage("정답이 아닙니다. 다시 시도해 주세요.");
+				}
+				setErrorMessage("권한이 없습니다. 로그인해주세요.");
 			});
 	};
 
@@ -59,12 +62,12 @@ export default function QuizModal({ code, question, onSuccess }: Props) {
 				<label className="text-2lg font-semibold text-gray-500">{question}</label>
 				<div className="relative mt-[10px]">
 					<input
-						className={`${isError ? "bg-red-100 outline-red-200" : "bg-gray-100"} w-full rounded-[10px] px-5 py-[10px] text-md font-normal text-gray-500 placeholder:text-gray-400`}
+						className={`${errorMessage !== "" ? "bg-red-100 outline-red-200" : "bg-gray-100"} w-full rounded-[10px] px-5 py-[10px] text-md font-normal text-gray-500 placeholder:text-gray-400`}
 						value={value}
 						onChange={(e) => setValue(e.target.value)}
 						placeholder="답안을 입력해 주세요"
 					/>
-					{isError && <p className="absolute mt-[10px] text-xs font-normal text-red-200">정답이 아닙니다. 다시 시도해 주세요.</p>}
+					{errorMessage !== "" && <p className="absolute mt-[10px] text-xs font-normal text-red-200">{errorMessage}</p>}
 				</div>
 				<div className="mt-10">
 					<Button onClick={handleButtonClick}>확인</Button>
