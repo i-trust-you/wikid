bfafc2fdb9f39afd22a2a311e6fe7085445c473d
thaqldks <sombian.software@gmail.com>
2024-07-05 14:18:36 +0900

    feat: header popover (#29)
    * first commit
    
    * fix: neflify build error
    
    * fix: 여백
    
    * fix: localStorage 오류
    
    * first commit
    
    * feat: popover menu
    
    ---------
    
    Co-authored-by: heony704 <heony704@gmail.com>

--- app/_components/common/Header.tsx
+++ app/_components/common/Header.tsx
@@ -5,6 +5,10 @@ import Link from "next/link";
 
 import useLocalStorage from "@/_hooks/useLocalStorage";
 
+import Popover from "@/_components/general/Popover";
+
+import MenuIcon from "../../../public/icons/MenuIcon";
+
 export default function Header() {
 	const [accessToken, setAccessToken] = useLocalStorage<string | null>("accessToken", null);
 
@@ -14,24 +18,54 @@ export default function Header() {
 				<Link href="/">
 					<Image src="/icons/logo.svg" alt="logo" width={105} height={30} />
 				</Link>
-				<Link href="/wikilist" className="tablet:visible">
+				<Link href="/wikilist" className="hidden tablet:block">
 					위키 목록
 				</Link>
-				<Link href="/boards" className="tablet:visible">
+				<Link href="/boards" className="hidden tablet:block">
 					자유게시판
 				</Link>
 			</div>
 			<div className="flex items-center gap-[24px]">
 				{accessToken ? (
 					<>
-						<Image src="/icons/alarm.svg" alt="logo" width={32} height={32} />
-						<Image src="/icons/profile.svg" alt="logo" width={32} height={32} />
+						<Link href="/notifications" className="hidden tablet:block">
+							<Image src="/icons/alarm.svg" alt="logo" width={32} height={32} />
+						</Link>
+						<Link href="/mypage" className="hidden tablet:block">
+							<Image src="/icons/profile.svg" alt="logo" width={32} height={32} />
+						</Link>
 					</>
 				) : (
-					<Link href="/login" className="text-gray-400">
+					<Link href="/login" className="hidden text-gray-400 tablet:block">
 						로그인
 					</Link>
 				)}
+				<Popover
+					gap={5}
+					trigger="click"
+					position="top-right-bottom-right"
+					overlay={
+						<div className="flex w-[120px] flex-col items-center overflow-hidden rounded-[10px] bg-white shadow-[0px_4px_8px_0px_#00000014]">
+							<div className="flex h-[44px] w-full items-center justify-center hover:bg-gray-100">알림</div>
+							<Link href="/wikilist">
+								<div className="flex h-[44px] w-full items-center justify-center hover:bg-gray-100">위키목록</div>
+							</Link>
+							<Link href="/boards">
+								<div className="flex h-[44px] w-full items-center justify-center hover:bg-gray-100">자유게시판</div>
+							</Link>
+							<Link href="/mypage">
+								<div className="flex h-[44px] w-full items-center justify-center hover:bg-gray-100">마이페이지</div>
+							</Link>
+							<div className="flex h-[44px] w-full items-center justify-center hover:bg-gray-100" onClick={() => setAccessToken(null)}>
+								로그아웃
+							</div>
+						</div>
+					}
+				>
+					<div className="tablet:hidden">
+						<MenuIcon width="24" height="24"></MenuIcon>
+					</div>
+				</Popover>
 			</div>
 		</header>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,46 @@
+import Popover from "@/_components/general/Popover";
+
 export default function Page() {
-	return <></>;
+	const overlay = (
+		<div className="flex w-[120px] flex-col items-center border border-red-200 bg-white shadow-lg">
+			<div>위키목록</div>
+			<div>자유게시판</div>
+			<div>알림</div>
+			<div>마이페이지</div>
+		</div>
+	);
+	return (
+		<main className="flex h-screen flex-col items-center justify-center gap-[10px]">
+			<Popover gap={5} trigger="click" position="top-right-top-left" overlay={overlay}>
+				<div className="h-[24px] w-[24px] border border-red-200"></div>
+			</Popover>
+			<Popover gap={5} trigger="click" position="top-right-bottom-left" overlay={overlay}>
+				<div className="h-[24px] w-[24px] border border-red-200"></div>
+			</Popover>
+			<Popover gap={5} trigger="click" position="bottom-right-top-left" overlay={overlay}>
+				<div className="h-[24px] w-[24px] border border-red-200"></div>
+			</Popover>
+			<Popover gap={5} trigger="click" position="bottom-right-bottom-left" overlay={overlay}>
+				<div className="h-[24px] w-[24px] border border-red-200"></div>
+			</Popover>
+			<Popover gap={5} trigger="click" position="top-left-top-right" overlay={overlay}>
+				<div className="h-[24px] w-[24px] border border-red-200"></div>
+			</Popover>
+			<Popover gap={5} trigger="click" position="top-left-bottom-right" overlay={overlay}>
+				<div className="h-[24px] w-[24px] border border-red-200"></div>
+			</Popover>
+			<Popover gap={5} trigger="click" position="bottom-left-top-right" overlay={overlay}>
+				<div className="h-[24px] w-[24px] border border-red-200"></div>
+			</Popover>
+			<Popover gap={5} trigger="click" position="bottom-left-bottom-right" overlay={overlay}>
+				<div className="h-[24px] w-[24px] border border-red-200"></div>
+			</Popover>
+			<Popover gap={5} trigger="click" position="top-left-bottom-left" overlay={overlay}>
+				<div className="h-[24px] w-[24px] border border-red-200"></div>
+			</Popover>
+			<Popover gap={5} trigger="click" position="top-right-bottom-right" overlay={overlay}>
+				<div className="h-[24px] w-[24px] border border-red-200"></div>
+			</Popover>
+		</main>
+	);
 }
