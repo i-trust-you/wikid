413047145bfa1a5562be3de41108c9301f70ca35
dotw0xff <154291841+dotw0xff@users.noreply.github.com>
2024-07-07 20:35:56 +0900

    Revert "게시글 상세 페이지의 폼 기능 구현 (#32)"
    This reverts commit b8e0f642870fcb1be03bfe7207ead2ce306bdcde.

--- app/boards/[id]/_components/CommentList.tsx
+++ app/boards/[id]/_components/CommentList.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import API from "@/_api";
-import { useEffect, useRef, useState } from "react";
-
-import useObserver from "@/_hooks/useObserver";
+import { useEffect, useState } from "react";
 
 import Comment from "./Comment";
-import CommentForm from "./CommentForm";
 
 type CommentType = Awaited<ReturnType<(typeof API)["{teamId}/articles/{articleId}/comments"]["GET"]>>["list"][0];
 
@@ -29,20 +26,7 @@ export default function CommentList({ articleId }: { articleId: number }) {
 			setCommentData([...commentData, ...value.list]);
 			setNextComment(value.nextCursor ?? 0);
 		});
-	}, [articleId]);
-
-	useObserver(
-		target,
-		0.3,
-		function getNextCommentData() {
-			if (nextComment === 0) return;
-			API["{teamId}/articles/{articleId}/comments"].GET({ teamId: "6-11", articleId, limit: 3, cursor: nextComment }).then((value) => {
-				setCommentData([...commentData, ...value.list]);
-				setNextComment(value.nextCursor ?? 0);
-			});
-		},
-		[nextComment],
-	);
+	};
 
 	return (
 		<div className="flex flex-col gap-2">
@@ -50,20 +34,13 @@ export default function CommentList({ articleId }: { articleId: number }) {
 				<span className="text-gray-500">댓글</span>
 				<span className="text-primary-200">{commentData ? commentData.length : 0}</span>
 			</div>
-			<CommentForm articleId={articleId} />
+			{/* // TODO: Form 컴포넌트 <div>Form</div> */}
+			{/* // TODO: Intersection Observer를 활용한 무한스크롤의 구현 */}
 			<div className="m-auto flex w-[335px] flex-col gap-[14px] tablet:w-[624px] tablet:gap-4 desktop:w-[1060px] desktop:gap-6">
 				{commentData && commentData.length > 0 ? (
-					<>
-						{commentData.map((comment) => (
-							<Comment key={comment.id} comment={comment} />
-						))}
-						<div ref={target} className="invisible h-[128px]"></div>
-					</>
+					commentData.map((comment) => <Comment key={comment.id} comment={comment} />)
 				) : (
-					<div
-						ref={target}
-						className="flex h-[128px] items-center justify-center rounded-[10px] font-normal text-gray-400 shadow-basic tablet:h-[134px] tablet:text-xl desktop:h-[136px]"
-					>
+					<div className="flex h-[128px] items-center justify-center rounded-[10px] font-normal text-gray-400 shadow-[0px_4px_20px_0px_rgba(0,0,0,0.1)] tablet:h-[134px] tablet:text-xl desktop:h-[136px]">
 						작성된 댓글이 없습니다.
 					</div>
 				)}
